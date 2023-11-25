694a2a322123913e8f8ec32d98e9b7017a0c9fb4
Lander Van laer <lander.vanlaer1@gmail.com>
2023-11-25 13:35:39 +0100

    Add UserWriteDto DataAnnotations

--- c-sharp-dotnet-development-lab-3AI-project/database/entities/user/dto/UserWriteDto.cs
+++ c-sharp-dotnet-development-lab-3AI-project/database/entities/user/dto/UserWriteDto.cs
@@ -1,7 +1,23 @@
-﻿namespace c_sharp_dotnet_development_lab_3AI_project.database.entities.user.dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace c_sharp_dotnet_development_lab_3AI_project.database.entities.user.dto;
 
 public class UserWriteDto
 {
+    [Required]
+    [StringLength(18,
+        ErrorMessage = "The field {0} must be a string with a minimum length of {2} and a maximum length of {1}.",
+        MinimumLength = 6)]
+    [RegularExpression("^[a-zA-Z0-9]+$",
+        ErrorMessage = "The field {0} must be a string with only letters and numbers.")]
     public string Username { get; set; }
+
+    [Required]
+    [StringLength(32,
+        ErrorMessage = "The field {0} must be a string with a minimum length of {2} and a maximum length of {1}.",
+        MinimumLength = 8)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$",
+        ErrorMessage =
+            "The field {0} must have at least one uppercase letter, one lowercase letter, one number and one special character.")]
     public string Password { get; set; }
 }
