3b7f82287f4e18fd8e75ca3155c287cfd5e0b148
Lander Van laer <lander.vanlaer1@gmail.com>
2023-12-13 16:53:24 +0100

    Add Refresh button to RefreshViews

---
+++ desktopapplication/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace desktopapplication.Converters;
+
+public class InvertBooleanConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // ReSharper disable once InvertIf
+        if (value is not bool boolean)
+        {
+            Debug.WriteLine("Please only use this converter with boolean values");
+            return false;
+        }
+
+        return !boolean;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        Convert(value, targetType, parameter, culture);
+}
